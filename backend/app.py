32e36ff89d4430532a11d0d46864a5327bcda536
Pratyashi Nath <pratyashin@gmai.com>
2026-04-07 20:40:19 +0530

    Prepare backend for deployment

--- backend/app.py
+++ backend/app.py
@@ -62,4 +62,5 @@ def delete_transaction(id):
     return jsonify({"message": "Deleted successfully"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == "__main__":
+      app.run()
